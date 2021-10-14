11aefcbafde6d8143be9ccfa600c9e0d033b4380
sam3177 <sam3177@users.noreply.github.com>
2021-10-14 08:34:01 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.89e9a6b5e8333d654db6d126b7a366fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c4a858ad5ef81df9ac540bd67b4c0b",
+    "url": "https://sam3177.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "94115513ae773bedbe3d",
+    "url": "https://sam3177.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "91f6df762a54bdc62ca9",
+    "url": "https://sam3177.github.io/task-management-frontend/static/js/2.f755303c.chunk.js"
+  },
+  {
+    "revision": "94115513ae773bedbe3d",
+    "url": "https://sam3177.github.io/task-management-frontend/static/js/main.29f0e974.chunk.js"
+  },
+  {
+    "revision": "e09665c8fd3657b9a1b9",
+    "url": "https://sam3177.github.io/task-management-frontend/static/js/runtime~main.3c5caa31.js"
+  }
+]);
